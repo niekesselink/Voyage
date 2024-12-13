042efee4e33ac9dac7bf9e70cfaa4541b7ab70d2
Niek Esselink <niek@posteo.net>
2024-12-14 00:17:17 +0100

    Clean-up some files

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
-
     document.getElementById('start').onclick = function () {
         chrome.runtime.sendMessage({ msg: 'voyageStart@' + document.getElementById('url').value });
     };
